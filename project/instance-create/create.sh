b535fe3a596df8647f1c02af30606ff446dd6963
avinash <avi78935@gmail.com>
2021-04-08 01:09:54 +0530

    created dns record file

--- project/instance-create/create.sh
+++ project/instance-create/create.sh
@@ -21,5 +21,6 @@ fi
 IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
 sed -e "s/COMPONENT/${COMPONENT}/"  -e"s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z10174611DOMQBMGYNMVO --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z10174611DOMQBMGYNMVO --change-batch file:///tmp/record.json
+
 

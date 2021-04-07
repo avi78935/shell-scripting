612b5f1022267840ddfed459d97358526e773589
avinash <avi78935@gmail.com>
2021-04-08 00:17:23 +0530

    created instance create

---
+++ project/instance-create/create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LID=lt-0d744f151b5a9254c
+LVER=2
+
+COMPONENT=$1
+
+if[ -z $1 ]; then
+  echo "Component Name is Required"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}

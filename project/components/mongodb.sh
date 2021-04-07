a2a83d9e4546a196c918a13b2f521a73fc841909
avinash <avi78935@gmail.com>
2021-04-07 14:56:36 +0530

    created mongodb and written shell

---
+++ project/components/mongodb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source components/common.sh
+
+PRINT "Setup Mongo Repository"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STAT $? "Setting Mongo repository"
+
+PRINT "Install mongodb"
+yum install -y mongodb-org
+STAT $? "Installing Mongodb"
+
+PRINT "Update MongoDB Configuration File"
+sed -i -e '/s/127.0.0.1/0.0.0.0' /etc/mongod.conf
+STAT $? "Updating MongoDB Configuration File"
+
+PRINT "Start mongodb"
+systemctl enable mongod
+systemctl restart mongod
+STAT $? "Starting MongoDB"
+
+PRINT "Download MongoDB Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+STAT $? "Downloading Schema"
+
+PRINT "Extract MongoDB Schemas"
+cd /tmp
+unzip -o mongodb.zip
+STAT $? "Extracting MongoDB schemas"
+
+PRINT "Load Schema"
+cd mongodb-main
+mongo < catalogue.js && mongo < users.js
+STAT $? "Loading Schema"
---
+++ project/components/practice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e "\e[1,32mHello World\e[0m"
